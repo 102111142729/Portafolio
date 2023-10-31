19ed9b6254633be091487667e10e482dd316d216
Albert Mario Zaharia <137610575+102111142729@users.noreply.github.com>
2023-10-31 15:20:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,6 @@ botonAnterior.addEventListener("click", mostrarImagenAnterior);
 // Mostrar la primera imagen al cargar la página
 mostrarImagen(indiceActual);
 
-const iconoMenu = document.querySelector(".icono-menu");
-const opcionesMenu = document.querySelector(".opciones-menu");
-
 iconoMenu.addEventListener("click", () => {
     opcionesMenu.classList.toggle("visible");
 });
